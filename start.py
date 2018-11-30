abca327a91d013ecdb56af3a8575eec2572d4cef
F. LABERENNE <flaberenne1@claranet.fr>
2018-11-30 23:00:16 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,9 @@
 #Let's start with something basic
 txt='Hello World'
 print(txt)
-# and then let's play 
+# and then let's play !!!
+# reverse
 print(txt[::-1])
+# every 3 starting from the end 
+print(txt[::-3])
+
